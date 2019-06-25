cfbba45857dae70e6428384ad963da8833e691e4
Lynn Griffin <amber.griffin@alaskaair.com>
2019-06-25 08:42:54 -0700

    Created urls.py and a babel file. Django in working state. Able to make calls.

--- adiemanager/frontend/urls.py
+++ adiemanager/frontend/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import views
-
-urlspatterns = [
-    path('', views.index)
-]
+# from django.urls import path
+# from . import views
+#
+# urlspatterns = [
+#     path('', views.index)
+# ]
